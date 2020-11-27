9389e1069b3f639fb68d41b97a31dadec8629316
RezaFirouzii <sr2000f@gmail.com>
2020-11-27 21:24:58 +0330

    adding validation for register & nodemailer module.

--- routes/entry-routes/forgot-password.js
+++ routes/entry-routes/forgot-password.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../config/db');
+const usersCollection = '_users';
 
 /* GET forgot password page. */
 router.get('/', function(req, res, next) {
     res.render('entry/forgot-password');
+});
+
+/* POST email (forgot password) */
+router.post('/', (req, res) => {
+    db.getDB().collection(usersCollection).find(req.body).toArray((err, documents) => {
+        if (err) console.log(err);
+        else if (documents.length) {
+            const securityCode = Math.floor(Math.random() * 100000) + 10000;
+            // we should now send the security code to user!
+        } else {
+            res.json({
+                ok: 0,
+                n: 0,
+                reason: 'No user found with the provided email.'
+            });
+        }
+    })
 });
 
 module.exports = router;
--- routes/entry-routes/register.js
+++ routes/entry-routes/register.js
@@ -4,20 +4,122 @@ const db = require('../../config/db');
 const usersCollection = '_users';
 
 /* GET register page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res) {
     res.render('entry/register');
 });
 
 /* POST register form */
 router.post('/', (req, res) => {
 
-    req.body.username = req.body.username[0] === '@'
-    ? req.body.username : "@" + req.body.username;
+    let endOfResponse = false;
 
-    db.getDB().collection(usersCollection).insertOne(req.body, (err, data) => {
-        if (err) console.log(err);
-        else res.json(data.result);
+    const validateObject = validate(req);
+    if (!validateObject.valid) {
+        res.json(validateObject);
+        return;
+    }
+
+    hasDuplicateEmail(req).then(response => {
+        return JSON.parse(JSON.stringify(response));
+    }).then(jsonResponse => {
+        if (jsonResponse.duplicate) {
+            res.json(jsonResponse);
+            endOfResponse = true;
+        }
+    }).catch(err => {
+        res.json(err);
+    });
+
+    req.body.username = req.body.username[0] === '@'
+        ? req.body.username : "@" + req.body.username;
+
+    hasDuplicateUsername(req).then(response => {
+        return JSON.parse(JSON.stringify(response));
+    }).then(jsonResponse => {
+        if (endOfResponse) return;
+        if (jsonResponse.duplicate) {
+            res.json(jsonResponse);
+        } else {
+            db.getDB().collection(usersCollection).insertOne(req.body, (err, data) => {
+                if (err) console.log(err);
+                else res.json(data.result);
+            });
+        }
+    }).catch(err => {
+        res.json(err);
     });
 });
+
+function validate(req) {
+
+    req.body.email.toLowerCase();
+    req.body.username.toLowerCase();
+
+    if (!(req.body.email.includes('@') && req.body.email.includes('.')
+        && req.body.email.indexOf('@') < req.body.email.lastIndexOf('.'))) {
+        return {
+            ok: 0,
+            n: 0,
+            valid: false,
+            reason: "Email is not valid!"
+        }
+    }
+
+    if (req.body.username.length < 5) {
+        return {
+            ok: 0,
+            n: 0,
+            valid: false,
+            reason: "Username must be at least 5 characters!"
+        }
+    }
+    return {
+        valid: true
+    }
+}
+
+function hasDuplicateEmail(req) {
+    let duplicateObject = {
+        duplicate: false
+    }
+    return new Promise((resolve, reject) => {
+        db.getDB().collection(usersCollection).find({email: req.body.email}).toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                return reject(err);
+            } else if (documents.length) {
+                duplicateObject = {
+                    ok: 0,
+                    n: 0,
+                    reason: `An account with the email: ${req.body.email} already exists!`,
+                    duplicate: true
+                }
+            }
+            return resolve(duplicateObject);
+        });
+    });
+}
+
+function hasDuplicateUsername(req) {
+    let duplicateObject = {
+        duplicate: false
+    }
+    return new Promise((resolve, reject) => {
+        db.getDB().collection(usersCollection).find({username: req.body.username}).toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                return reject(err);
+            } else if (documents.length) {
+                duplicateObject = {
+                    ok: 0,
+                    n: 0,
+                    reason: `The username: ${req.body.username} is already copied!`,
+                    duplicate: true
+                }
+            }
+            return resolve(duplicateObject);
+        });
+    });
+}
 
 module.exports = router;
