0d24cf982d7c81e2836d7fc7678563cff2124f91
RezaFirouzii <sr2000f@gmail.com>
2020-12-02 00:02:58 +0330

    Login page

--- routes/entry-routes/login.js
+++ routes/entry-routes/login.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../config/db');
+const usersCollection = '_users';
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
-    res.render('entry/login');
-    req.session.destroy(err => {
+router.get('/', function (req, res, next) {
+    if (req.session.valid) {
+        console.log('Ready to enter the chat!'); // Redirection to chat page
+    } else req.session.destroy(err => {
         if (err) console.log(err);
+    });
+    res.render('entry/login');
+});
+
+/* POST login request */
+router.post('/', (req, res) => {
+    console.log(req.body)
+    db.getDB().collection(usersCollection).find(req.body).toArray((err, documents) => {
+        if (err) console.log(err);
+        else if (documents.length) {
+            req.session.valid = true;
+            req.session.user = documents[0];
+            res.json({
+                ok: 1
+            });
+        } else res.json({
+                ok: 0,
+                description: "username or password is wrong!\n\nTry again."
+            });
     });
 });
 
